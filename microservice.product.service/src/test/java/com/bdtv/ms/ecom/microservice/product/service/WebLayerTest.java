027bd37bda9f5735d5beb49c2133687bcaf39d84
Dillon <Dillon.Zhang@smith.co>
2018-04-24 13:28:39 +0800

    try spring restdocs mockmvc

---
+++ microservice.product.service/src/test/java/com/bdtv/ms/ecom/microservice/product/service/WebLayerTest.java
@@ -0,0 +1,31 @@
+package com.bdtv.ms.ecom.microservice.product.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.bdtv.ms.ecom.product.service.controller.ProductController;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/productapi/products")).andDo(print()).andExpect(status().isOk())
+                .andDo(document("product"));
+    }
+}
